a2b852be75e0d0990bc3fff682b2d970354ec145
Moealsir <mohamedwdalsir@gmail.com>
2023-11-28 23:39:53 +0300

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for char in range(ord('a'), ord('z') + 1):
-    if chr(char) not in ['e', 'q']:
-        print("{}".format(chr(char)), end="")
-        
+for ch in range(97, 123):
+    print("{:c}".format(ch), end='')
