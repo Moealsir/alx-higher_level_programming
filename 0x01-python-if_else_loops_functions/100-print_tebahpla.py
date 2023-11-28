f2436a6d4b1ac170dd2875ccb744c7715e0b969b
Moealsir <mohamedwdalsir@gmail.com>
2023-11-29 02:41:37 +0300

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2:
-        i = i + 32
-    print("{:c}".format(i), end='')
+for char in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(char)), end="")
+    char -= 32  
