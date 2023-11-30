803d38e0e597a3602a8935d0a83da6b2e2f7d42c
Moealsir <mohamedwdalsir@gmail.com>
2023-11-30 23:16:17 +0300

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+    from calculator_1 import *
     import sys
     nargs = len(sys.argv) - 1
     if nargs != 3:
