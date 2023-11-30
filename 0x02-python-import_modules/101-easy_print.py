1510c74cd8ad8f6edd4fe580f531dcf437f6b988
Moealsir <mohamedwdalsir@gmail.com>
2023-11-30 23:37:25 +0300

    101-easy_print.py

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from whew import printer
+    a = "#pythoniscool"
+    printer(a)
---
+++ 0x02-python-import_modules/whew.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def printer(p):
+    return(print(p))
