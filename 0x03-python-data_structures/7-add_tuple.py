5664fc36310344be35b060828fba5a09a9f11dfd
Moealsir <mohamedwdalsir@gmail.com>
2023-12-04 16:36:24 +0300

    6-print_matrix_integer.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    len_a = len(tuple_a)
-    len_b = len(tuple_b)
-
+    len_a = tuple_a
+    len_b = tuple_b
     if len_a == 0:
         a1 = 0
         a2 = 0
-    elif len_a == 1:
+    elif len == 1:
         a1 = tuple_a[0]
         a2 = 0
     else:
@@ -16,7 +15,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len_b == 0:
         b1 = 0
         b2 = 0
-    elif len_b == 1:
+    elif len == 1:
         b1 = tuple_b[0]
         b2 = 0
     else:
@@ -24,5 +23,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         b2 = tuple_b[1]
 
     new_tuple = (a1 + b1, a2 + b2)
-
-    return new_tuple
+    return (new_tuple)
