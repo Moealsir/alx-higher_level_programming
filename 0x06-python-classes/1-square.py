f3f3a3d22bb23f9979394c5321299b83d643810d
Moealsir <mohamedwdalsir@gmail.com>
2023-12-20 00:56:44 +0300

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-    """a module of a Square class
-    """
+"""a module of a Square class
+"""
 
 
 class Square:
     """a class that define square
-    
+
     Attributes:
         __size: square size
     """
