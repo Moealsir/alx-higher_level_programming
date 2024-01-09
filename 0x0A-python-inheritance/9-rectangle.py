1aea2d664fc0a55238b233f4df323a85d0e27c65
Moealsir <mohamedwdalsir@gmail.com>
2024-01-09 04:06:51 +0300

    modified descs

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+"""
+summary_
+"""
 
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 """
 Rectangle module
 """
