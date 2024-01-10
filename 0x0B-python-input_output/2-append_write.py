2ed263c53283aca855a408a63be37579d69e8608
Moealsir <mohamedwdalsir@gmail.com>
2024-01-10 04:02:58 +0300

    4-from_json_string.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+whew
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    whew
+    """
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
+
--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import json
+"""
+whew
+"""
+
+
+def to_json_string(my_obj):
+    """
+    whew
+    """
+    
+    return json.dump(my_obj)
