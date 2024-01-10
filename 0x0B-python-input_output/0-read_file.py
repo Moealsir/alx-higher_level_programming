e699109210cb0767780011502aa3f98f075c0b08
Moealsir <mohamedwdalsir@gmail.com>
2024-01-10 03:41:26 +0300

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, endcoding='utf-8') as myfile:
-        print(myfile.read())
+    with open(filename, 'r', endcoding='utf-8') as myfile:
+        print(myfile.read(), end='')
