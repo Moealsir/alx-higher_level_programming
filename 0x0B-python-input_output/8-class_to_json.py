7a799fd195a02c645ae2296cb69d430ca882d4b5
Moealsir <mohamedwdalsir@gmail.com>
2024-01-10 04:43:59 +0300

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""
+whew
+"""
+
+def class_to_json(obj):
+    """
+    whew
+    """
+    
+    res = {}
+    if hasattr(obj, "__dist__"):
+        res = obj.__dist__.copy()
+        return res
