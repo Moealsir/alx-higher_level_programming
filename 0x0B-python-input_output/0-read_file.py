243730d18b55a5c345d9db1791135da13b0f49d6
Moealsir <mohamedwdalsir@gmail.com>
2024-01-10 03:51:39 +0300

    1-write_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,7 +8,7 @@ def read_file(filename=""):
     """
     whew
     """
-    
+
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end='')
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+whew
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    whew
+    """
+
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
