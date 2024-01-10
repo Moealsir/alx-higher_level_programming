fb5458724dd3e754361a75e0410e22ed5202ae9d
Moealsir <mohamedwdalsir@gmail.com>
2024-01-10 05:09:28 +0300

    11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+"""
+whew
+"""
+
+
+class Student:
+    """
+    A class representing a student.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        whew
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        whew
+        """
+        obj = self.__dict__.copy()
+        if type(attrs) is not str:
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+        
+        d_list = {}
+        
+        for iatr in range(len(attrs)):
+            for satr in obj:
+                d_list[satr] = obj[satr]
+        return d_list
+    
+    def reload_from_json(self, json):
+        """
+        whew
+        """
+        for atr in json:
+            self.__dict__[atr] = json[atr]
