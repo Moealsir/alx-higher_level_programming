d0d2c311cc245d76301a1baa1abf2a24f8271e21
Moealsir <mohamedwdalsir@gmail.com>
2024-01-10 04:57:40 +0300

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -8,7 +8,7 @@ class Student:
     """
     A class representing a student.
     """
-    
+
     def __init__(self, first_name, last_name, age):
         """
         whew
@@ -16,7 +16,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self):
         """
         whew
