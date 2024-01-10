5196951a387ed935e6f1555d0b5721c6ca54e278
Moealsir <mohamedwdalsir@gmail.com>
2024-01-10 04:30:13 +0300

    5-save_to_json_file.py

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+whew
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    whew
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
