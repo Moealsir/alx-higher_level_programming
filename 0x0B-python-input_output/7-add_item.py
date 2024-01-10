c640c62273f22b9df00aff072a6b60f9dfd6aa3a
Moealsir <mohamedwdalsir@gmail.com>
2024-01-10 04:49:55 +0300

    8-class_to_json.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Module that adds all arguments to a Python list, and then
+"""
+Module that adds all arguments to a Python list, and then
 save them to a file
 """
 import sys
