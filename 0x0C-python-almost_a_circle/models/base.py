63f6256e9b9cf787822f9f2a6b38e5282845e9a8
Moealsir <mohamedwdalsir@gmail.com>
2024-01-16 01:01:17 +0300

    t1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,10 +20,27 @@ class Base:
         Constructor for the Base class.
 
         Args:
-            id (int, optional): An optional identifier.
+            id (int, optional): An optional.
         """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+
+if __name__ == "__main__":
+    b1 = Base()
+    print(b1.id)
+
+    b2 = Base()
+    print(b2.id)
+
+    b3 = Base()
+    print(b3.id)
+
+    b4 = Base(12)
+    print(b4.id)
+
+    b5 = Base()
+    print(b5.id)
