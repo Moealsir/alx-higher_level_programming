81c060c5bf283e43c8bf4175cd6eb861668a57a3
Moealsir <mohamedwdalsir@gmail.com>
2024-01-16 01:57:57 +0300

    t2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,78 +1,91 @@
 #!/usr/bin/python3
-"""Module for the Rectangle class"""
+"""
+Rectangle(Base) module
+"""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """The Rectangle class"""
+    """
+    Rectangle(Base) - Add a brief here.
+    """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        Constructor for the Rectangle class.
-
-        Args:
-            width (int): Width of the rectangle.
-            height (int): Height of the rectangle.
-            x (int, optional): x-coordinate of the rectangle. Defaults to 0.
-            y (int, optional): y-coordinate of the rectangle. Defaults to 0.
-            id (int, optional): Identifier for the instance. Defaults to None.
+        __init__ - Add a brief here.
         """
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-
     @property
-    def width(self):
-        """Getter method for __width"""
-        return self.__width
 
+    def width(self):
+        """
+        width - Add a brief here.
+        """
+        return self.__width
     @width.setter
+
     def width(self, value):
-        """Setter method for __width"""
+        """
+        width - Add a brief here.
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
-
     @property
-    def height(self):
-        """Getter method for __height"""
-        return self.__height
 
+    def height(self):
+        """
+        height - Add a brief here.
+        """
+        return self.__height
     @height.setter
+
     def height(self, value):
-        """Setter method for __height"""
+        """
+        height - Add a brief here.
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
-
     @property
-    def x(self):
-        """Getter method for __x"""
-        return self.__x
 
+    def x(self):
+        """
+        x - Add a brief here.
+        """
+        return self.__x
     @x.setter
+
     def x(self, value):
-        """Setter method for __x"""
+        """
+        x - Add a brief here.
+        """
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
-
     @property
-    def y(self):
-        """Getter method for __y"""
-        return self.__y
 
+    def y(self):
+        """
+        y - Add a brief here.
+        """
+        return self.__y
     @y.setter
+
     def y(self, value):
-        """Setter method for __y"""
+        """
+        y - Add a brief here.
+        """
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
         if value < 0:
