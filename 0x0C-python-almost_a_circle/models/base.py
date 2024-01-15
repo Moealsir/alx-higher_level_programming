bec26296ebecdd637e37c2444f15c5cd855df1de
Moealsir <mohamedwdalsir@gmail.com>
2024-01-16 01:03:32 +0300

    t1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,46 +1,22 @@
 #!/usr/bin/python3
+
 """
-Module for the Base class
+Base module
 """
 
 
 class Base:
     """
-    The Base class
-
-    Attributes:
-        __nb_objects (int): A private class attribute.
-        id (int): A public instance attribute.
+    Base - Add a brief here.
     """
 
-    __nb_objects = 0
 
     def __init__(self, id=None):
         """
-        Constructor for the Base class.
-
-        Args:
-            id (int, optional): An optional.
+        __init__ - Add a brief here.
         """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-
-
-if __name__ == "__main__":
-    b1 = Base()
-    print(b1.id)
-
-    b2 = Base()
-    print(b2.id)
-
-    b3 = Base()
-    print(b3.id)
-
-    b4 = Base(12)
-    print(b4.id)
-
-    b5 = Base()
-    print(b5.id)
