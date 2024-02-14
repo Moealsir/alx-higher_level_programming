3016f0f4f2ae0071ed76d7c85ee7ee4d3bbd8505
Moealsir <mohamedwdalsir@gmail.com>
2024-02-15 02:24:41 +0300

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- t
 -- u
-GRANT ALL PRIVILEGES *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-GRANT ALL PRIVILEGES . TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
