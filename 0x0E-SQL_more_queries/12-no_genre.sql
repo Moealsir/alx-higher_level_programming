a010be8592f38d50f09c04409effe273be32c287
Moealsir <mohamedwdalsir@gmail.com>
2024-02-15 23:59:37 +0300

    12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -5,4 +5,4 @@ from tv_shows
 LEFT JOIN tv_show_genres ON
 tv_shows.id = tv_show_genres.genre_id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
