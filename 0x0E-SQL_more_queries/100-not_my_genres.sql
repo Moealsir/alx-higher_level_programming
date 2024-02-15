932c6b5c2b4af2dd124a4725fcda4274920b930e
Moealsir <mohamedwdalsir@gmail.com>
2024-02-16 00:36:57 +0300

    100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,12 +1,13 @@
 -- t
 -- u
-SELECT tv_genres.name
+SELECT DISTINCT tv_genres.name
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
-WHERE NOT EXISTS (
-    SELECT 1
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.id NOT IN (
+    SELECT tv_show_genres.genre_id
     FROM tv_show_genres
     JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-    WHERE tv_shows.title = 'Dexter' && tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_shows.title = 'Dexter'
 )
 ORDER BY tv_genres.name ASC;
+
