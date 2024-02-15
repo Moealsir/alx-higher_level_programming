0067d086aa926f2fce66ba5e121e21f826697955
Moealsir <mohamedwdalsir@gmail.com>
2024-02-16 00:41:11 +0300

    101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -5,7 +5,7 @@ FROM tv_shows
 WHERE NOT EXISTS (
     SELECT 1
     FROM tv_show_genres
-    JOIN tv_genres ON iv_show_genres.genre_id = tv_genres.id
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
     WHERE tv_show_genres.id = tv_shows.id
     AND tv_genres.name = 'Comedy'
 )
