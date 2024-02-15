f7f6d3ffbc0a6d55b0a1738b88770d852e6a1e73
Moealsir <mohamedwdalsir@gmail.com>
2024-02-16 00:42:03 +0300

    101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -6,7 +6,7 @@ WHERE NOT EXISTS (
     SELECT 1
     FROM tv_show_genres
     JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-    WHERE tv_show_genres.id = tv_shows.id
+    WHERE tv_show_genres.show_id = tv_shows.id
     AND tv_genres.name = 'Comedy'
 )
 ORDER BY tv_shows.title ASC;
