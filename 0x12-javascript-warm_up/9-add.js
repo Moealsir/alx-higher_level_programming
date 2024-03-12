f0c4a2054498c62f666be5c69915afc1f3a48c66
Moealsir <mohamedwdalsir@gmail.com>
2024-03-13 01:48:27 +0300

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+// add
+const firstInt = parseInt(process.argv[2]);
+const secInt = parseInt(process.argv[3]);
+
+if (isNaN(firstInt) || isNaN(secInt)) {
+  console.log('NaN');
+} else {
+  const sum = firstInt + secInt;
+  console.log(sum);
+}
