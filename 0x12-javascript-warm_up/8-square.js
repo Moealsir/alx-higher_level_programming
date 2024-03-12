43aec498ed9f009fb801fec86250f84cdf1c2206
Moealsir <mohamedwdalsir@gmail.com>
2024-03-12 23:59:32 +0300

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 // square
 const size = parseInt(process.argv[2]);
 
-if (isNaN(size) || size < 0) {
+if (isNaN(size) || size < 1) {
   console.log();
 } else {
   for (let i = 0; i < size; i++) { console.log('X'.repeat(size)); }
