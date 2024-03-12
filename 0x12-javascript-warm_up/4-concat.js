e180e62a067d7f25e17226266e13ea419bba1147
Moealsir <mohamedwdalsir@gmail.com>
2024-03-12 23:35:40 +0300

    4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,3 @@
 #!/usr/bin/node
 // concat
-if (process.argv[2] === undefined || process.argv[3] === undefined) {
-  console.log(process.argv[2], 'is', process.argv[3]);
-} else {
-  console.log(process.argv[2], 'is', process.argv[3]);
-}
+console.log(process.argv[2], 'is', process.argv[3]);
