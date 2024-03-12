fbefe2a10bccc048bd5ebe6f6c4cb5d4b4963b3f
Moealsir <mohamedwdalsir@gmail.com>
2024-03-13 02:13:06 +0300

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 // Second biggest!
-
 const args = process.argv.slice(2).map(Number);
 
-if (args.length === 0 || args.length === 1) {
-  console.log(0);
-} else {
-  const sortedArgs = args.sort((a, b) => b - a);
-
-  console.log(sortedArgs[1]);
+if (args.length === 0 || args.length === 1) { console.log(0); } else {
+  const sortedargs = args.sort((a, b) => b - a);
+  console.log(sortedargs[1]);
 }
