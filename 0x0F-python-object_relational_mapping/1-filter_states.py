8b385b14c4245deb01679bca694c5bd1fc1614e9
Moealsir <mohamedwdalsir@gmail.com>
2024-03-16 02:39:23 +0300

    1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""list states"""
+import MySQLdb
+import sys
+
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: script.py <username> <password> <database>")
+        sys.exit(1)
+    username, password, database = sys.argv[1:]
+    username = 'root'
+    password = '1111'
+    database = 'hbtn_0e_0_usa'
+    conn = MySQLdb.connect(
+        host="localhost", user=username, password=password, db=database
+    )
+    cur = conn.cursor()
+    cur.execute(
+        "SELECT * FROM states \
+            WHERE name LIKE 'N%' \
+            ORDER BY id ASC")
+    items = cur.fetchall()
+    for item in items:
+        print(item)
+    cur.close()
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
