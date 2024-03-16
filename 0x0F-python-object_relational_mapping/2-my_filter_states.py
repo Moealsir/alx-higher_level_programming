2af94b61a7a75af8616fa92e656eeb59f73e94f4
Moealsir <mohamedwdalsir@gmail.com>
2024-03-16 03:11:29 +0300

    2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,25 +1,50 @@
 #!/usr/bin/python3
-"""list states"""
 import MySQLdb
 import sys
 
-def main():
-    if len(sys.argv) < 3:
-        print("Usage: script.py <username> <password> <database>")
-        sys.exit(1)
-    username, password, database, state = sys.argv[1:]
-    conn = MySQLdb.connect(
-        host="localhost", user=username, password=password, db=database
-    )
-    cur = conn.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cur.execute(query, (state,))
-    items = cur.fetchall()
-    for item in items:
-        print(item)
-    cur.close()
-    conn.close()
-
-
 if __name__ == "__main__":
-    main()
+    # Check if all required arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get MySQL connection parameters
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL database:", e)
+        sys.exit(1)
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the query
+    try:
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+        cursor.execute(query, (state_name,))
+        results = cursor.fetchall()
+    except MySQLdb.Error as e:
+        print("Error executing query:", e)
+        cursor.close()
+        db.close()
+        sys.exit(1)
+
+    # Print the results
+    for row in results:
+        print(row)
+
+    # Close cursor and database connection
+    cursor.close()
+    db.close()
