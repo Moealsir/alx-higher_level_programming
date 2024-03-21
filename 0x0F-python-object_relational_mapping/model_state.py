11eb556038c682f0a9798c390f6d78a9a027c6cd
Moealsir <mohamedwdalsir@gmail.com>
2024-03-21 17:19:01 +0300

    7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""model for class state"""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class state(Base):
+    """database state"""
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    engine = create_engine('mysql://root:root@localhost:3306/hbtn_0e_4_usa')
