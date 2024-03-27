2578a4fe70043160fd1c1635befa9afbf8f984e8
Moealsir <mohamedwdalsir@gmail.com>
2024-03-28 01:08:32 +0300

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to the URL and displays the body of the response if the status code is 200
-curl -s -X GET -i "$1" | awk 'BEGIN {body=0} {if (body) print} /HTTP\/1.1 200 OK/{body=1}'
+# cURL body size
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
