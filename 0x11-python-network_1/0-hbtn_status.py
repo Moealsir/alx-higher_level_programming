305a51a96c59e51c72e1c7965c88fe9a091a0557
Moealsir <mohamedwdalsir@gmail.com>
2024-03-29 01:59:50 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
-import urllib
-import urllib.request
-"""Fetches and displays the body of the response"""
-
+#!/usr/bin/python3
+"""
+script that fetches https://intranet.hbtn.io/status
+"""
 if __name__ == "__main__":
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print("Body response:")
+    import urllib.request as request
+    with request.urlopen('https://intranet.hbtn.io/status') as ul:
+        html = ul.read()
+        print('Body response:')
         print("\t- type: {}".format(type(html)))
         print("\t- content: {}".format(html))
         print("\t- utf8 content: {}".format(html.decode('utf-8')))
