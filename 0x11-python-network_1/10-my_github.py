4e246beb56fbd20ab0254d5f3877163265d97224
Moealsir <mohamedwdalsir@gmail.com>
2024-03-29 04:55:50 +0300

    10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     url = 'https://api.github.com/user'
     response = requests.get(url, auth=(username, password))
     if response.status_code == 200:
-        print(response.json())
+        print(response.json()['id'])
     else:
         print(None)
+        
