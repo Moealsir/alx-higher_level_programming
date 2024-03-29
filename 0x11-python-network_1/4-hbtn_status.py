3985ef179fc96c8959f71b4dbf7a2dac50f5a987
Moealsir <mohamedwdalsir@gmail.com>
2024-03-29 04:34:25 +0300

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-    Fetches https://intranet.hbtn.io/status
-    using package 'requests'
-    Body response:$
-    - type: <class 'str'>$
-    - content: OK$
+Script that fetches https://alx-intranet.hbtn.io/status using requests.
 """
-
 import requests
 
+
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(r.text), r.text))
+    url = "https://alx-intranet.hbtn.io/status"
+    response = requests.get(url)
+    content = response.text
+
+    print("Body response:")
+    print("\t- type: ", type(content))
+    print("\t- content: ", content)
