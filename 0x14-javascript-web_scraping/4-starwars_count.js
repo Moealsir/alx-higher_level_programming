ee9ab4af20379c03d822b8531f7a0a0c90898b22
Moealsir <mohamedwdalsir@gmail.com>
2024-04-23 19:09:55 +0300

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,37 +1,17 @@
 #!/usr/bin/node
-// test
+const { get } = require('request');
+const { argv } = require('process');
+const url = argv[2];
 
-const request = require('request');
-const apiUrl = process.argv[2];
-const characterID = '18';
-
-function countMoviesWithWedge (apiUrl, characterID, callback) {
-  request(apiUrl, (error, response, body) => {
-    if (error) {
-      callback(new Error(error), null);
-      return;
-    }
-    if (response.statusCode !== 200) {
-      callback(new Error(`Failed to fetch movie data. Status code: ${response.statusCode}`), null);
-      return;
-    }
-    const films = JSON.parse(body).results;
-    let count = 0;
-    films.forEach((film) => {
-      const characters = film.characters;
-      if (characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterID}/`)) {
-        count++;
-      }
-    });
-    callback(null, count);
-  });
-}
-
-// Main execution
-countMoviesWithWedge(apiUrl, characterID, (error, count) => {
-  if (error) {
-    console.error(error.message);
+get(url, (err, res, data) => {
+  if (err) {
+    console.log(err);
     return;
   }
-  console.log(count);
+
+  const { results } = JSON.parse(data);
+  const count = [0, ...results]
+    .reduce((prevr, { characters }) => prevr + [0, ...characters]
+      .reduce((prevc, character) => prevc + character.includes('18')));
+  console.log(isNaN(count) ? 0 : count);
 });
