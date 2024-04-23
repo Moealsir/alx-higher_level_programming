6d5f605def75455873b175c6af439c999e5d9001
Moealsir <mohamedwdalsir@gmail.com>
2024-04-23 18:24:27 +0300

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -7,10 +7,9 @@ const { argv } = require('process');
 const filename = argv[2];
 const content = argv[3];
 
-fs.writeFile(filename, content, 'utf-8', (err) => {
+fs.writeFile(filename, content, err => {
   if (err) {
     console.log(err);
     return;
   }
-  console.log(content);
 });
